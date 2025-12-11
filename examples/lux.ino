6a52e22c069ee78c7f2bae2ec468e8a8103ff29b
Raidho <124537911+Rthrall@users.noreply.github.com>
2025-12-10 16:09:16 -0800

    Rename lux object to lux300x and update references

--- examples/lux.ino
+++ examples/lux.ino
@@ -2,7 +2,7 @@
 #include <opt300x.h>
 
 // Create lux object
-opt300x lux;
+opt300x lux300x;
 
 // I2C address (0x44, 0x45, 0x46, or 0x47)
 const uint8_t I2C_ADDRESS = 0x44;
@@ -14,22 +14,22 @@ void setup() {
   Wire.begin();
   
   // Setup the OPT300X (I2C library, I2C address)
-  if (lux.setup(Wire, I2C_ADDRESS) != 0) {
+  if (lux300x.setup(Wire, I2C_ADDRESS) != 0) {
     Serial.println("Failed to setup OPT300X");
     return;
   }
   
   // Detect the lux
-  if (lux.detect() != 0) {
+  if (lux300x.detect() != 0) {
     Serial.println("OPT300X not detected");
     return;
   }
   
   // Configure conversion time (100ms or 800ms)
-  lux.config_set(OPT300X_CONVERSION_TIME_800MS);
+  lux300x.config_set(OPT300X_CONVERSION_TIME_800MS);
   
   // Enable continuous conversion mode
-  lux.conversion_continuous_enable();
+  lux300x.conversion_continuous_enable();
   
   Serial.println("OPT300X initialized");
 }
@@ -38,7 +38,7 @@ void loop() {
   float lux;
   
   // Read illuminance in lux
-  if (lux.lux_read(&lux) == 0) {
+  if (lux300x.lux_read(&lux) == 0) {
     Serial.print("Illuminance: ");
     Serial.print(lux);
     Serial.println(" lux");
